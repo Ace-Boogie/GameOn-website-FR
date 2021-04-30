70ef5f134563a3106f7f99cef64e6e82f555df54
Ace-Boogie <desbonnes.marvin@gmail.com>
2021-04-30 09:45:16 +0200

    version 1.1.0

--- starterOnly/public/js/modal.js
+++ starterOnly/public/js/modal.js
@@ -10,25 +10,45 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalClose = document.getElementById("closeModal");
+const checkCondition = document.getElementById("checkbox1");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const modalLabels = document.querySelectorAll("label");
 const formData = document.querySelectorAll(".formData");
-const inputRequireds = document.querySelectorAll("input[required]");
 const textLabel = document.querySelector(".text-label");
 const btnPostData = document.getElementById("btnPostData");
+const formTag = document.querySelector("form");
 const birthdate = document.getElementById("birthdate");
-// btnPostData.disabled = true;
-let resultBirthday = [];
-let resultCitys;
-let resultEmails;
-let resultFirsts;
-let resultLasts;
-let resultBirthdates;
-let resultGames;
-let resultData = [];
-let infoRed = document.createElement('p');
-let infoEmpty = document.createElement('p');
 
+const btnPostClose = document.createElement("input");
+btnPostClose.classList.add("btn-submit", "button");
+btnPostClose.type = "submit";
+btnPostClose.value = "Close";
+
+const textValidation = document.createElement("h3");
+textValidation.style.textAlign = "center";
+textValidation.innerHTML = "Merci,<br> Votre réservation a bien été enregistrée"
+
+let resultFinal = [];
+
+let resultCitys;
+let resultCitysTrue;
+
+let resultEmails;
+let resultEmailsTrue;
+
+let resultFirsts;
+let resultFirstsTrue;
+
+let resultLasts;
+let resultLastsTrue;
+
+let resultBirthdates;
+let resultBirthdatesTrue;
+
+let resultGames;
+let resultGamesTrue;
+
+let resultConditions;
 
 
 // Pattern des différents types
@@ -45,14 +65,20 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
     modalbg.style.display = "block";
+    if (checkCondition.hasAttribute("checked")) {
+        return resultConditions = true;
+    }
 }
 
 // //close modal form
 const closeModal = (closed) => {
-    closed.addEventListener("click", function closeModal() {
-        console.log("modal fermé");
-        modalbg.style.display = "none";
-    })
+    // if (closed === modalClose || btnPostData.value === "Close") {
+    if (closed === modalClose) {
+        closed.addEventListener("click", function closeModal() {
+            console.log("modal fermé");
+            modalbg.style.display = "none";
+        })
+    }
 }
 closeModal(modalClose);
 
@@ -65,11 +91,9 @@ function formsData(event) {
     // Validation des patterns
     const validatePattern = (type, pattern) => {
         if (event.target.type === type && event.target.value.match(pattern)) {
-            // console.log("c'est vert");
-            // btnPostData.disabled = false;
-            return event.target.style.border = "4px solid green";
+            event.target.style.border = "4px solid green";
         } else if (event.target.type === type && !event.target.value.match(pattern)) {
-            return event.target.style.border = "4px solid red";
+            event.target.style.border = "4px solid red";
         }
     }
     validatePattern("date", dateFormat);
@@ -81,31 +105,51 @@ function formsData(event) {
         if (event.target.value === modalLabel.innerText) {
             resultCitys = textLabel.innerText + " : " + event.target.value;
             console.log(resultCitys);
+            return resultCitysTrue = true;
 
         } else if (event.target.id === modalLabel.getAttribute("for")) {
             if (event.target.id === "birthdate" && event.target.value.match(dateFormat)) {
                 resultBirthdates = modalLabel.innerText + " : " + event.target.value;
                 console.log(resultBirthdates);
+                return resultBirthdatesTrue = true;
             }
             if (event.target.id === "email" && event.target.value.match(emailFormat)) {
                 resultEmails = modalLabel.innerText + " : " + event.target.value;
                 console.log(resultEmails);
+                return resultEmailsTrue = true;
 
             }
-            if (event.target.id === "quantity" && event.target.value.match(numberFormat)) {
-                resultGames = modalLabel.innerText + " : " + event.target.value;
-                console.log(resultGames);
-
+            if (event.target.id === "quantity") {
+                if (event.target.value.match(numberFormat)) {
+                    resultGames = modalLabel.innerText + " : " + event.target.value;
+                    console.log(resultGames);
+                    return resultGamesTrue = true;
+                } else {
+                    return resultGamesTrue = false;
+                }
             }
+
             if (event.target.id === "first" && event.target.value.match(textFormat)) {
                 resultFirsts = modalLabel.innerText + " : " + event.target.value;
                 console.log(resultFirsts);
-
+                return resultFirstsTrue = true;
             }
             if (event.target.id === "last" && event.target.value.match(textFormat)) {
                 resultLasts = modalLabel.innerText + " : " + event.target.value;
                 console.log(resultLasts);
+                return resultLastsTrue = true;
 
+            }
+            if (event.target.id === "checkbox1") {
+                if (event.target.checked) {
+                    event.target.value = "ok";
+                    console.log(event.target.value);
+                    return resultConditions = true;
+                } else {
+                    event.target.value = "off";
+                    console.log(event.target.value);
+                    return resultConditions = false;
+                }
             }
         }
 
@@ -114,28 +158,45 @@ function formsData(event) {
 
 }
 
-// Envoyer les données et acccusé de réception
-
-// function validate(event) {
 btnPostData.addEventListener("click", function validateData(event) {
-        event.preventDefault();
-for (let inputRequired of inputRequireds){
+    event.stopPropagation();
 
-    if (inputRequired.style.border === "4px solid red"){
-        console.log("il y a du rouge");
-        infoRed.textContent ="Erreur dans la saisie";
-        inputRequired.after(infoRed);
-        // alert("");
-    } else if (inputRequired.style.border === "4px solid green" && inputRequired.style.border === "4px solid red"){
-        console.log("il y a un problème");
-        alert("Une ou plusieurs saisies ne sont pas bonne");
-    } else if (inputRequired.style.border === ""){
-        console.log("il y a un second problème");
-        infoEmpty.textContent ="Le champs est vide";
-        inputRequired.after(infoEmpty);
-    } else if (inputRequired.style.border === "4px solid green"){
-        console.log("c'est validé");
+    if (resultFirstsTrue === true && resultLastsTrue === true && resultEmailsTrue === true && resultBirthdatesTrue === true
+        && resultCitysTrue === true && resultGamesTrue === true && resultConditions === true) {
+        resultFinal.push(resultFirsts, resultLasts, resultEmails, resultBirthdates, resultCitys, resultGames);
+        console.log(resultFinal);
+        if (textLabel.innerHTML === "Quelles villes ?") {
+            textLabel.classList.add("formOpacity");
+        }
+        for (let addOpacity in formData) {
+            if (formData.hasOwnProperty(addOpacity)) {
+                formData[addOpacity].classList.add("formOpacity");
+            }
+        }
+        btnPostData.style.display = "none";
+        formTag.appendChild(btnPostClose);
+        formData[3].after(textValidation)
     }
-    // console.log(inputRequired.style.border);
-}
+});
+
+btnPostClose.addEventListener("click", function validate(event) {
+    console.log("modal fermé via btn");
+    if (textLabel.innerHTML === "Quelles villes ?") {
+        textLabel.classList.remove("formOpacity");
+    }
+    for (let removeOpacity in formData) {
+        if (formData.hasOwnProperty(removeOpacity)) {
+            formData[removeOpacity].classList.remove("formOpacity");
+        }
+    }
+    modalbg.style.display = "none";
+    btnPostData.style.display = "block";
+    formTag.removeChild(btnPostClose);
+    textValidation.textContent = "";
+    resultFinal = [];
+    console.log(resultFinal);
+
 })
+
+
+
